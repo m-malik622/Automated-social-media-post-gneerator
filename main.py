7b445ff54733f9e9eb524ff98508d45e4cbf61ee
Malik <144081337+m-malik622@users.noreply.github.com>
2024-02-15 16:30:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os #for enviornment variables if you would like to have one for the api_key
-from Search_For_Youtube_Video import *
-from Download_Youtube_Vid import *
-from Overlap_Videos import *
+from Social_Media_Post_Automation.Search_For_Youtube_Video import *
+from Social_Media_Post_Automation.Download_Youtube_Vid import *
+from Social_Media_Post_Automation.Overlap_Videos import *
 
 
 def Social_Media_Post_Automation(api_key: str, top_vid_query_terms: list[str], num_of_top_vids: int, bottom_vid_query_terms: list[str], num_of_bottom_vids: int, top_vid_download_destination: str, bottom_vid_download_destination: str, result_vid_path:str, start_time:int, end_time:int, have_top_vid_stretched:bool):
-     """
+    """
     Automates the process of creating social media posts by overlaying top and bottom videos.
 
     Args:
@@ -23,10 +23,13 @@ def Social_Media_Post_Automation(api_key: str, top_vid_query_terms: list[str], n
     Note:
         - The function is not fully implemented and requires additional functionality for accessing social media platforms.
         - It currently handles downloading top and bottom videos and overlaying them based on specified parameters.
-    """   
+    """
+
+
     top_video_links = Search_For_Youtube_Video(api_key, top_vid_query_terms, num_of_top_vids)
     for top_video_link in top_video_links:
         top_vid_path = Download_Youtube_Vid(top_video_link, top_vid_download_destination)
+
     print("the top video has been succesfully downloaded")
 
     bottom_video_links = Search_For_Youtube_Video(api_key, bottom_vid_query_terms, num_of_bottom_vids)
